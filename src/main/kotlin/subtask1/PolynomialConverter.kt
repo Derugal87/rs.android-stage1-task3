085a94ca5ab624b63d35d29b0bed2da68b83909a
Derugal87 <60985658+Derugal87@users.noreply.github.com>
2020-05-06 23:35:52 +0300

    Update PolynomialConverter.kt

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,33 @@
 package subtask1
 
+import kotlin.math.absoluteValue
+
 class PolynomialConverter {
 
-    // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        if (numbers.size == 0) return null
+        var result = ""
+        var coef = numbers.size
+        for(i in numbers.indices){
+            if(numbers[i]==0){result += "" }
+            else if (numbers[i]==-1) {  result+= " - "+"x"}
+            else if(numbers[i]==1) {  result+= " + "+"x"}
+            else if(numbers[i]>0) {
+                if(i == coef - 1) {
+                    result+= " + "+numbers[i].absoluteValue.toString()
+                } else if (i == coef - 2) {
+                    result+= " + "+numbers[i].absoluteValue.toString()+"x"
+                }
+                else if(numbers[i]==numbers.first()){
+                    result+= numbers[i].absoluteValue.toString()+"x^"+(coef-i-1).toString()
+                }
+                else{
+                    result+= " + "+numbers[i].absoluteValue.toString()+"x^"+(coef-i-1).toString()
+                }
+            }
+            else if(numbers[i]<0) {  result+= " - "+numbers[i].absoluteValue.toString()+"x^"+(coef-i-1).toString()}
+
+        }
+        return result
     }
 }
